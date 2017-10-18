77c6fb6c07167fbdd56c8f405d6917b596d61fdb
Travis CI <travis@travis-ci.org>
2017-10-17 19:30:49 -0700

    add echos

--- push.sh
+++ push.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
 setup_git() {
+  echo "Setting Git config ..."
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
 commit_website_files() {
-  git checkout -b push-test
+  echo "Making commit $TRAVIS_BUILD_NUMBER ..."
+  git checkout -b auto-branch
   git add . contributors.md
   git add . requirements.txt
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
+  echo "Pushing to GitHub ..."
   git remote add origin https://${GITHUB_TOKEN}@github.com/cltk/cltk.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin push-test
+  git push --quiet --set-upstream origin auto-branch
 }
 
 setup_git
